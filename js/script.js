4358722bf5064a526b4163955a0afd6a6cc14518
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-05-12 00:26:36 +0000

    Done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+
+"use strict";
+
+/**
+ * This function converts Fahrenheit to Celsius
+ */
+function convertFahrenheitToCelsius() {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit-input').value);
+
+  // process
+  const celsius = ((fahrenheit - 32) * 5 / 9).toFixed(3);
+
+  // output
+  document.getElementById('celsius-result').innerHTML = 'Celsius: ' + celsius;
+}
